4847bf60fe281f5cefd009871700d57e8c1711f9
oandreazini <oriol_as_92@hotmail.com>
2022-07-07 13:13:34 +0200

    Second Commit

---
+++ TSystems_UD5/src/Ej2App.java
@@ -0,0 +1,12 @@
+
+public class Ej2App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nombre = "Oriol Andreazini";
+		
+		System.out.println("Bienvenido " +nombre);
+	}
+
+}
