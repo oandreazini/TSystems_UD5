f04dd3a6cd289808a29ba6b801211ecbe7cbc880
oandreazini <oriol_as_92@hotmail.com>
2022-07-07 16:12:25 +0200

    Fourth Commit

---
+++ TSystems_UD5/src/Ej4App.java
@@ -0,0 +1,26 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Ej4App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double area;
+		//double radio;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduce el radio del circulo:");
+		
+		String radio = sc.nextLine();
+		sc.close();
+		
+		double numRadio = Double.parseDouble(radio);
+		System.out.println("El radio introducido es: " +numRadio);
+		
+		area= Math.PI*(Math.pow(numRadio, 2));
+		System.out.printf("El área del circulo es: %.2f", area);
+	}
+
+}
