6c1e4cf692b50dc57bbd0921da76d19d372c504f
oandreazini <oriol_as_92@hotmail.com>
2022-07-07 13:08:05 +0200

    First Commit

---
+++ TSystems_UD5/src/Ej1App.java
@@ -0,0 +1,25 @@
+
+public class Ej1App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int A = 500;
+		int B = 50;
+		
+		if (A<B) {
+			System.out.println("El valor mayor es la variable B");
+		} else {
+			if(A>B) {
+				System.out.println("El valor mayor es la variable A");
+			} else {
+				if(A==B) {
+					System.out.println("Las variables tienen el mismo valor");
+
+				}
+			}
+		}
+		
+	}
+
+}
