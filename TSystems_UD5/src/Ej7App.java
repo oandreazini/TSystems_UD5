e080c2a319e357901c452ba02e3433e98b50ac83
oandreazini <oriol_as_92@hotmail.com>
2022-07-07 16:56:25 +0200

    Seventh Commit

---
+++ TSystems_UD5/src/Ej7App.java
@@ -0,0 +1,16 @@
+
+public class Ej7App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num=0;
+		
+		while(num<101) {
+			System.out.println(num);
+			num++;
+		}
+
+	}
+
+}
